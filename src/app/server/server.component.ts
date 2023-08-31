5ee33627eb728dc53a46e74cd6df2e734c910151
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-08-31 09:23:01 +0630

    testing the selector usage

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // method 1
   selector: 'app-server',
+  // method 2
+  // selector: '[app-server]',
+  // method 3
+  // selector: '.app-server',
   templateUrl: './server.component.html',
 })
 export class ServerComponent {}

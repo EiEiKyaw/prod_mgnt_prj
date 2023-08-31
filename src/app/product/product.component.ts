5180739667a6450a5d4a3b689f843cc311b20b3c
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-08-31 14:17:37 +0630

    two-way binding

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
   allowNewProduct = false;
   productCreationStatus = 'No new product creates.';
+  productName = 'Hello World';
 
   constructor() {
     setTimeout(() => {
@@ -21,5 +22,9 @@ export class ProductComponent implements OnInit {
 
   onCreateProduct() {
     this.productCreationStatus = 'New product was created.';
+  }
+
+  onUpdateProduct(event: Event) {
+    this.productName = (<HTMLInputElement>event.target).value;
   }
 }

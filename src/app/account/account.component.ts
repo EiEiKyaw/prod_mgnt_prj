be4fa6c49cbb585569f64c9cbb3c9c473846c9b6
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-09-04 16:59:04 +0630

    practice

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -3,20 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  // styleUrls: ['./account.component.css'],
-  styles: [
-    `
-      .online {
-        background-color: skyblue;
-      }
-    `,
-  ],
+  styleUrls: ['./account.component.css'],
+  // styles: [
+  //   `
+  //     .online {
+  //       background-color: skyblue;
+  //     }
+  //   `,
+  // ],
 })
 export class AccountComponent {
   accountStatus = 'offline';
+  btnStatus = false;
+  countClick: any[] = [];
 
   constructor() {
     this.accountStatus = Math.random() < 0.5 ? 'offline' : 'online';
+  }
+
+  onToggleBtn() {
+    this.btnStatus = !this.btnStatus;
+    this.countClick.push(this.countClick.length + 1);
+  }
+
+  getBtnColor() {
+    return this.btnStatus ? 'green' : 'orange';
   }
 
   getColor() {

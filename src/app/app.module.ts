1f88cac78fd5a47a68d280034ffef90b83a5931a
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-09-06 16:18:06 +0630

    add recipe-list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -15,6 +16,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipesListComponent,
     RecipesDetailComponent,
     RecipesItemComponent,
     ShoppingListComponent,
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe One',
+      'This is testing 1',
+      'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/1:1/w_1920,c_limit/04102023-ratatouille-lede.jpg'
+    ),
+    new Recipe(
+      'Recipe Two',
+      'This is testing 2',
+      'https://assets.epicurious.com/photos/63ea6ba855007bc33c26ba0b/4:3/w_4862,h_3647,c_limit/Shakshuka_RECIPE_020923_47096.jpg'
+    ),
+  ];
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }

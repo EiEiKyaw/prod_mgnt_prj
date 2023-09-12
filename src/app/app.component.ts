08ee086b1bb0352f44d298b4adb06409c509c576
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-09-12 13:18:04 +0630

    bind the element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'prod_mgnt_prj';
+  serverElements = [
+    { type: 'server', name: 'Test Server', content: 'Just a test!' },
+  ];
 }
